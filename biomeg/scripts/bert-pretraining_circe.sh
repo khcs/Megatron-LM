d6981eb375e88ab71ae9abace4924a18c7cdcfbb
Hoo Shin <hshin@nvidia.com>
2020-04-21 04:07:16 -0700

    add draco and circe run scripts.

---
+++ biomeg/scripts/bert-pretraining_circe.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+LOGNAME="${L_NAME}"
+
+cd /workspace/MegatronLM/
+
+MP_SIZE=1
+
+CHECKPOINT_PATH="${CKPT_PATH}"
+VOCAB_FILE="${V_FILE}"
+DATA_PATH="${D_PATH}"
+
+BERT_ARGS="--num-layers 24 \
+           --hidden-size 1024 \
+           --num-attention-heads 16 \
+           --seq-length 512 \
+           --max-position-embeddings 512 \
+           --lr 0.0001 \
+           --train-iters 2000000 \
+           --min-lr 0.00001 \
+           --lr-decay-iters 990000 \
+           --warmup 0.01 \
+           --batch-size 8 \
+           --vocab-file $VOCAB_FILE \
+           --split 949,50,1 \
+           --fp16"
+
+OUTPUT_ARGS="--log-interval 10 \
+             --save-interval 500 \
+             --eval-interval 100 \
+             --eval-iters 10 \
+             --checkpoint-activations"
+
+python pretrain_bert.py \
+        $BERT_ARGS \
+        $OUTPUT_ARGS \
+        --save $CHECKPOINT_PATH \
+        --load $CHECKPOINT_PATH \
+        --data-path $DATA_PATH \
+        --local_rank=${SLURM_LOCALID} \
+        --model-parallel-size $MP_SIZE \
+        --DDP-impl torch \
+        2>&1 | tee bert_pretraining-${LOGNAME}.log
